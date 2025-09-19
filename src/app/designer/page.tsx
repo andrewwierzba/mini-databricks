75c821d198d1972ad0c2223346a2549585088d87
Andrew Wierzba <andrewwierzba@gmail.com>
2025-09-19 17:37:53 +0200

    Add designer directory

---
+++ src/app/designer/page.tsx
@@ -0,0 +1,10 @@
+import { Workspace } from "@/components/ui/patterns/workspace-browser"
+
+export default function Home() {
+    return (
+        <div className="bg-(--du-bois-color-background-primary) border-(--du-bois-color-border) border rounded-sm flex font-sans h-full">
+            <Workspace />
+            Mini Databricks
+        </div>
+    );
+}
