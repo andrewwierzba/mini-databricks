8a886d301891a2d8b151ce3639ab7b336f3dccd1
Andrew Wierzba <andrewwierzba@gmail.com>
2025-09-19 17:31:38 +0200

    Rename file-browser to workspace-browser

---
+++ src/components/ui/patterns/workspace-browser.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+
+import { ArrowLeftIcon, CloseIcon, DataIcon, FileIcon, FolderIcon, FolderFillIcon, QueryEditorIcon, Typography } from "@databricks/design-system"
+
+const { Text } = Typography
+
+const data = [{
+    name: "folder_1",
+    type: "folder"
+}, {
+    name: "folder_2",
+    type: "folder"
+}, {
+    name: "folder_3",
+    type: "folder"
+}, {
+    name: "folder_4",
+    type: "folder"
+}]
+
+export function FileBrowser({ onClose }: { onClose: () => void }) {
+    return (
+        <div aria-label="file-browser" className="border-r border-(--du-bois-color-border) h-full min-w-[300px] p-1">
+            <div className="items-center flex justify-between w-full">
+                <Button
+                    aria-label="navigate-outside-of-folder"
+                    className="rounded-sm"
+                    size="icon"
+                    variant="ghost"
+                >
+                    <ArrowLeftIcon />
+                </Button>
+                <Typography className="mb-[2px]">
+                    <Text bold>minidatabricks@databricks.com</Text>
+                </Typography>
+                <Button
+                    aria-label="close-file-browser"
+                    className="rounded-sm"
+                    onClick={onClose}
+                    size="icon"
+                    variant="ghost"
+                >
+                    <CloseIcon />
+                </Button>
+            </div>
+            <div className="items-center flex gap-2 h-6 px-3">
+                <FolderFillIcon style={{ color: 'var(--du-bois-text-secondary)' }} />
+                <Typography>
+                    <Text className="mb-[2px]">Drafts</Text>
+                </Typography>
+            </div>
+            { data.map((object, i) => {
+                return (
+                    <div className="items-center flex gap-2 h-6 px-3" key={i}>
+                        {object.type === 'file' ? (
+                            <FileIcon style={{ color: 'var(--du-bois-text-secondary)' }} />
+                        ) : (
+                            <FolderFillIcon style={{ color: 'var(--du-bois-color-blue-400)' }} />
+                        )}
+                        <Typography>
+                            <Text className="mb-[2px]">{object.name}</Text>
+                        </Typography>
+                    </div>
+                )
+            }) }
+        </div>
+    )
+}
+
+export function Workspace() {
+    const [showFileBrowser, setShowFileBrowser] = useState(false)
+    
+    return (
+        <>
+            <div aria-label="file browser" className="border-r border-(--du-bois-color-border) flex flex-col h-full">
+                {/* Workspace menu */}
+                <div className="flex flex-col gap-1 p-1">
+                    <Button
+                        aria-label="view-query-editor"
+                        className="rounded-sm"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <QueryEditorIcon />
+                    </Button>
+                </div>
+                <Separator />
+                <div className="flex flex-col gap-1 p-1">
+                    <Button
+                        aria-label="view-file-browser"
+                        className="rounded-sm"
+                        size="icon"
+                        variant="ghost"
+                        onClick={() => setShowFileBrowser(!showFileBrowser)}
+                    >
+                        <FolderIcon />
+                    </Button>
+                    <Button
+                        aria-label="view-catalog"
+                        className="rounded-sm"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <DataIcon />
+                    </Button>
+                </div>
+            </div>
+            {showFileBrowser && <FileBrowser onClose={() => setShowFileBrowser(false)} />}
+        </>
+    )
+}
