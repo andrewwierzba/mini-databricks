b2d2c472cd9a2bced59f72d89a38a758ab6aa644
Andrew Wierzba <andrewwierzba@gmail.com>
2025-09-22 13:39:01 +0200

    Add toolbar to designer

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
+import { ReactFlow, Background, Controls, MiniMap } from "reactflow"
+import "reactflow/dist/style.css"
+
 import { useState } from "react"
 import { Navigation } from "@/components/ui/patterns/navigation"
 import { Workspace } from "@/components/ui/patterns/workspace-browser"
 import { Assistant } from "@/components/ui/patterns/assistant"
 
-import { ReactFlow, Background, Controls, MiniMap } from 'reactflow'
-import 'reactflow/dist/style.css'
+import { Toolbar } from "@/app/designer/components/toolbar"
 
 export default function Designer() {
     const [showAssistant, setShowAssistant] = useState(true)
@@ -40,37 +42,40 @@ export default function Designer() {
             <div className="bg-(--du-bois-color-background-primary) border-(--du-bois-color-border) border rounded-sm flex font-sans h-full overflow-hidden">
                 <Workspace />
                 <div className="flex-1 h-full">
-                    <ReactFlow
-                        className="bg-(--du-bois-color-background-primary)"
-                        edges={initialEdges}
-                        fitView
-                        nodes={initialNodes}
-                        panOnDrag={false}
-                        panOnScroll={true}
-                        selectionOnDrag={true}
-                    >
-                        <Background gap={8} />
-                        <Controls position="bottom-right" />
-                        <MiniMap
-                            bgColor="var(--du-bois-background-primary)"
-                            maskColor={undefined}
-                            maskStrokeColor="var(--du-bois-border)"
-                            maskStrokeWidth={1}
-                            nodeBorderRadius={20}
-                            nodeColor="var(--du-bois-background-primary)"
-                            nodeStrokeColor="var(--du-bois-border)"
-                            nodeStrokeWidth={10}
-                            position="bottom-right" 
-                            style={{ 
-                                border: "1px solid var(--du-bois-border)",
-                                borderRadius: "4px",
-                                height: 80,
-                                overflow: "hidden",
-                                right: 36,
-                                width: 138
-                            }}
-                        />
-                    </ReactFlow>
+                    <div aria-label="designer-canvas" className="h-full relative">
+                        <ReactFlow
+                            className="bg-(--du-bois-color-background-primary)"
+                            edges={initialEdges}
+                            fitView
+                            nodes={initialNodes}
+                            panOnDrag={false}
+                            panOnScroll={true}
+                            selectionOnDrag={true}
+                        >
+                            <Background gap={8} />
+                            <Controls position="bottom-right" />
+                            <MiniMap
+                                bgColor="var(--du-bois-background-primary)"
+                                maskColor={undefined}
+                                maskStrokeColor="var(--du-bois-border)"
+                                maskStrokeWidth={1}
+                                nodeBorderRadius={20}
+                                nodeColor="var(--du-bois-background-primary)"
+                                nodeStrokeColor="var(--du-bois-border)"
+                                nodeStrokeWidth={10}
+                                position="bottom-right" 
+                                style={{ 
+                                    border: "1px solid var(--du-bois-border)",
+                                    borderRadius: "4px",
+                                    height: 80,
+                                    overflow: "hidden",
+                                    right: 36,
+                                    width: 138
+                                }}
+                            />
+                        </ReactFlow>
+                        <Toolbar className="left-0 absolute top-0" />
+                    </div>
                 </div>
                 {showAssistant && <Assistant onClose={() => setShowAssistant(false)} />}
             </div>
