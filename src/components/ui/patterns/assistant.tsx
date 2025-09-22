11ba0064b3e440cf299ad6fc2cb3e59fced4348f
Andrew Wierzba <andrewwierzba@gmail.com>
2025-09-22 11:56:36 +0200

    Add Assistant buttons and textarea

--- src/components/ui/patterns/assistant.tsx
+++ src/components/ui/patterns/assistant.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
 
-import { CloseIcon, Typography } from "@databricks/design-system"
+import { CloseIcon, GearIcon, OverflowIcon, PlusIcon, SendIcon, Typography } from "@databricks/design-system"
 
 const { Text } = Typography
 
@@ -13,10 +14,34 @@ export function Assistant({ onClose }: { onClose: () => void }) {
                 <Typography>
                     <Text bold>Assistant</Text>
                 </Typography>
-                <div className="flex justify-end w-full">
+                <div className="flex gap-1 justify-end w-full">
+                    <Button
+                        aria-label="new-assistant"
+                        className="rounded-sm h-6 w-6"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <PlusIcon />
+                    </Button>
+                    <Button
+                        aria-label="settings-assistant"
+                        className="rounded-sm h-6 w-6"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <GearIcon />
+                    </Button>
+                    <Button
+                        aria-label="more-assistant"
+                        className="rounded-sm h-6 w-6"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <OverflowIcon />
+                    </Button>
                     <Button
                         aria-label="close-assistant"
-                        className="rounded-sm"
+                        className="rounded-sm h-6 w-6"
                         onClick={onClose}
                         size="icon"
                         variant="ghost"
@@ -24,6 +49,21 @@ export function Assistant({ onClose }: { onClose: () => void }) {
                         <CloseIcon />
                     </Button>
                 </div>
+            </div>
+            <div className="h-full"></div>
+            <div className="flex-1 flex flex-col gap-2 mt-4 relative">
+                <Textarea 
+                    placeholder="@ for objects, / for commands, ↑↓ for history"
+                    className="flex-1 resize-none min-h-[100px]"
+                />
+                <Button
+                    aria-label="send-assistant"
+                    className="rounded-sm bottom-2 absolute right-2"
+                    size="icon"
+                    variant="ghost"
+                >
+                    <SendIcon />
+                </Button>
             </div>
         </div>
     )
