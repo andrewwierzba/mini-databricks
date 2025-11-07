e5f0f356ecf52df11d57fea25fcb71c65f72e5aa
Andrew Wierzba <andrewwierzba@gmail.com>
2025-11-07 11:52:34 +0100

    Use application shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { Navigation } from "@/components/ui/patterns/navigation"
-import { Assistant } from "@/components/ui/patterns/assistant"
+import { ApplicationShell } from "@/components/ui/patterns/application-shell";
 
 export default function Home() {
-	const [showAssistant, setShowAssistant] = useState(false)
-
 	return (
-		<div className="flex flex-col h-full">
-			<Navigation onOpenAssistant={() => setShowAssistant(true)} />
-			<div className="bg-(--du-bois-color-background-primary) border-(--du-bois-color-border) border rounded-sm flex font-sans h-full">
-				<div className="p-6 w-full">
-					<h1 className="text-4xl font-medium text-center w-full">Mini Databricks</h1>
-				</div>
-				{showAssistant && <Assistant onClose={() => setShowAssistant(false)} />}
+		<ApplicationShell>
+			<div className="p-6">
+				<h1 className="text-4xl font-medium text-center w-full">Mini Databricks</h1>
 			</div>
-		</div>
+		</ApplicationShell>
 	)
 }
