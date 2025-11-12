0e7f5d74c1e62f1c1559eb81908002dda87e4b54
Andrew Wierzba <andrewwierzba@gmail.com>
2025-11-12 15:28:01 +0100

    Update application shell

--- src/components/ui/patterns/application-shell.tsx
+++ src/components/ui/patterns/application-shell.tsx
@@ -13,19 +13,19 @@ interface NavigationItem {
     href: string;
     icon: React.ReactNode;
     name: string;
-}
+};
 
 interface ApplicationShellProps {
     children: React.ReactNode;
     navigationItems?: NavigationItem[];
-}
+};
 
 export function ApplicationShell({
     children,
     navigationItems,
 }: ApplicationShellProps) {
-    const [showAssistant, setShowAssistant] = useState(false)
-    const [showSideNavigation, setShowSideNavigation] = useState(false)
+    const [showAssistant, setShowAssistant] = useState(false);
+    const [showSideNavigation, setShowSideNavigation] = useState(false);
 
     return (
         <div className="flex flex-1 flex-col h-full p-2">
@@ -36,21 +36,21 @@ export function ApplicationShell({
                 onToggleSideNavigation={() => setShowSideNavigation(!showSideNavigation)}
             />
             
-			<div className="flex flex-1 h-full">
+			<div className="flex flex-1 overflow-hidden">
 				{/* Application Navigation */}
 				{showSideNavigation && <SideNavigation navigationItems={navigationItems} onClose={() => setShowSideNavigation(false)} />}
 				
-			{/* Application Content */}
-			<div className="bg-(--du-bois-color-background-primary) border-(--du-bois-color-border) border rounded-sm flex flex-1 font-sans overflow-hidden">
-				<div aria-label="application-content" className="flex-1 overflow-hidden">
-					{children}
-				</div>
-				{showAssistant && <Assistant onClose={() => setShowAssistant(false)} />}
-			</div>
+                {/* Application Content */}
+                <div className="bg-(--white-800) border-(--gray-100) border rounded-sm flex flex-1 font-sans overflow-hidden">
+                    <div aria-label="application-content" className="flex-1 overflow-hidden">
+                        {children}
+                    </div>
+                    {showAssistant && <Assistant onClose={() => setShowAssistant(false)} />}
+                </div>
 			</div>
 
             {/* Application Notifications */}
             <Toaster />
 		</div>
-    )
-}
+    );
+};
